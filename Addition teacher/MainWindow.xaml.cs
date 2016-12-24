739519acdd08a16f5b05d0fd6d8b6f1240241e62
gods1son <tolulopeibiyode@gmail.com>
2016-12-23 22:40:08 -0500

    addition tutor

---
+++ Addition teacher/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Addition_teacher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        VM vm;
+        public MainWindow()
+        {
+            InitializeComponent();
+            vm = new VM();
+            DataContext = vm;
+        }
+
+        private void btnCheck_Click(object sender, RoutedEventArgs e)
+        {
+            vm.docalc();
+        }
+
+        private void btnReveal_Click(object sender, RoutedEventArgs e)
+        {
+            vm.Reveal();
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            vm.Save();
+            MessageBox.Show("File saved");
+        }
+    }
+}
---
+++ Addition teacher/VM.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Addition_teacher
+{
+    class VM:INotifyPropertyChanged
+    {
+        Random r = new Random();
+        public int Rand1
+        {
+            get { return _rand1; }
+            set { _rand1 = value; OnPropertyChanged(); }
+        }
+        private int _rand1;
+        public int Rand2
+        {
+            get { return _rand2; }
+            set { _rand2 = value; OnPropertyChanged(); }
+        }
+        private int _rand2;
+        public int Addition
+        {
+            get { return _addition; }
+            set { _addition = value; OnPropertyChanged(); }
+        }
+        private int _addition;
+        public VM()
+        {
+            Generate();
+        }
+        public void Generate()
+        {           
+            Rand1 = r.Next(100, 501);
+            Rand2 = r.Next(100, 501);
+            
+        }
+        public void docalc()
+        {
+            int Add = Rand1 + Rand2;
+            if (Addition == Add)
+            {
+                System.Windows.MessageBox.Show("Congrats, U are Right!!!");
+                Generate();
+                Addition = 0;
+            }
+            else if (Addition != Add)
+            {
+                System.Windows.MessageBox.Show("Wrong, Try Again");
+            }
+        }
+        public void Reveal()
+        {
+            int Add = Rand1 + Rand2;
+            System.Windows.MessageBox.Show(Add.ToString());
+            Generate();
+            Addition = 0;
+        }
+        public string DIRNAME = "Tutor";
+        public string filename = "question.txt";
+        public void Save()
+        {
+            int Add = Rand1 + Rand2;
+            string path = System.Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            string fullpath = Path.Combine(path, DIRNAME);
+            Directory.CreateDirectory(fullpath);
+            string fullname = Path.Combine(fullpath, filename);
+            StreamWriter sw = File.AppendText(fullname);
+            sw.WriteLine(Rand1.ToString() + "+" );
+            sw.WriteLine(Rand2.ToString());
+            sw.WriteLine("Your answer was " + (Addition.ToString()));
+            sw.WriteLine("Correct answer is " + (Add.ToString()));
+            sw.Close();
+        }
+            
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
